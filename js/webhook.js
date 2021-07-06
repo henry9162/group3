0d773d535e843580f5c5c3331979daf96e74276e
henry9162 <henimastic@yahoo.com>
2021-07-06 07:23:17 +0100

    changed base url

--- js/webhook.js
+++ js/webhook.js
@@ -1,13 +1,18 @@
 $(document).ready(function () {
+    var currenyValue;
+    $('#currencyInput').on('click',function() {
+        currenyValue = $(this).val();
+    });
+
     $('.loader').hide();
     $('#success').hide();
     
     $('#submitBtn').click(function(){
-        savePost();
-    })
+        savePost(currenyValue);
+    });
 });
 
-function savePost(){
+function savePost(currenyValue){
     if($('#accountNumberInput1').val() != '' && $('#amountInput1').val() != '' && $('#referenceInput1').val() != ''){
         $('#submitBtn').hide()
         $('.loader').show()
@@ -23,12 +28,19 @@ function savePost(){
             Remarks: "Transfer",
             SenderName: "Unwana Uforo",
             SenderAccountNumber: "0051762787",
-            BankCode: "058"
+            BankCode: "058",
+            currency: currenyValue
         }
 
-        fetch("https://xdwfw9kou5.execute-api.eu-west-2.amazonaws.com/dev/payment/BankTransfer/VirtualTransferHook", {
+        console.log(data)
+
+        // var encryptedData = encrypt(JSON.stringify(data));
+        // console.log(encryptedData);
+        // debugger
+
+        fetch("https://9fhf3wc560.execute-api.eu-west-2.amazonaws.com/dev/payment/BankTransfer/VirtualTransferHook", {
             method: 'POST',
-            body: JSON.stringify(data),
+            body: data,
             headers: {
                 'Content-type': 'application/json; charset=UTF-8',
                 'Authorization': 'application/json; charset=UTF-8',
@@ -46,6 +58,14 @@ function savePost(){
             cleanUp(jsonResponse)
         })
     }
+}
+
+function encrypt(payload){
+    let publicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAyFP9D0KDMgszvbpt443hQtJPZNceMmYL/MJ47a9CTC/DUFF4rE27v2u9ZgRI+N8ImSKH3H+E9gEmV4Q5YB0bbJkCPxCRb+TqsmSgv0b7Es/vQcQXs4izLVxMTxUYOd1i51mDuewowYcUZ4p4M+t4fj+zol/FEj2GAnlMk9Z1ansvDx98ANFJP0Ipo/o2OREwk8wMfVGgNIzcLWQoNJRIZdJdehQBfkWgER2pdmSGYUc1bYijPB05OgrAJAvSAjIeMoQlR9aeWkNFUfSS5lw9R8Pn8NSVPjtYZUpVeE+4Nr5jnxw/WYjYtc+RgM3w7aTo/iCQvENLB826nbcSXYbxpQIDAQAB"
+    let RSAEncrypt = new JSEncrypt();
+        RSAEncrypt.setPublicKey(publicKey);
+    let encryptedPayload = RSAEncrypt.encrypt(payload);
+    return encryptedPayload;
 }
 
 function cleanUp(jsonResponse){
